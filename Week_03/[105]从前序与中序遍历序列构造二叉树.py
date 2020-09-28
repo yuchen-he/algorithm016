dccb1118bd56854218021901099c71dd121faac8
yuchen_he <heyuchen0425@gmail.com>
2020-09-28 11:26:13 +0900

    [add] week3_homeworks_rev2

--- Week_03/[105]从前序与中序遍历序列构造二叉树.py
+++ Week_03/[105]从前序与中序遍历序列构造二叉树.py
@@ -28,18 +28,48 @@
 #         self.right = None
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        if len(preorder) == 0:
+    def helper(self, preorder, p_start, p_end, inorder, i_start, i_end):
+        if p_start == p_end:
             return None
+        root = TreeNode(preorder[p_start])
+        # id_root = inorder.index(preorder[p_start])
+        # 解法三: 改进点
+        id_root = self.hash_map[preorder[p_start]]
+        left_num = id_root - i_start       # 该节点左子树的总节点个数 -> 用于在pre数组中从p_start+1开始截取left_pre数组
 
-        root = TreeNode(preorder[0])
-        id_root = inorder.index(preorder[0])      # O(N)？
-        # left_pre = preorder[1 : ]
-        # left_in = inorder[: id_root]
-        # right_pre = preorder[(id_root + 1) :]
-        # right_in = inorder[id_root :]
-        root.left = self.buildTree(preorder[1 : (id_root + 1)], inorder[: id_root])
-        root.right = self.buildTree(preorder[(id_root + 1) :], inorder[id_root + 1 :])
+        # p_start + left_num + 1 是关键 == left_pre数组的结束位置（见goodnote）
+        root.left = self.helper(preorder, p_start+1, p_start+left_num+1, inorder, i_start, id_root)
+        root.right = self.helper(preorder, p_start+left_num+1, p_end, inorder, id_root+1, i_end)
 
         return root
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        # 解法三: 维护一个inorder的hash_map(每个位置的值和index对应)，从而不用每次遍历inorder去找id_root
+        self.hash_map = {}
+        for idx, value in enumerate(inorder):
+            self.hash_map[value] = idx
+        # 解法二: 因为拆分pre和in数组需要耗费计算量，所以不每次拆分他们
+        # 精华: 改为传入其原数组，以及left_pre, left_in, right_pre, right_in在原pre和in中的的start和end位置(左闭右开)
+        return self.helper(preorder, 0, len(preorder), inorder, 0, len(inorder))
+
+        # 解法一
+        # 前序遍历的结果数组中，元素顺序为: root/左子树/右子树
+        # 中序遍历的结果数组中，元素顺序为: 左子树/root/右子树
+        # 所以找到root即可拆分左右子树，并且得到left_pre, left_in, right_pre, right_in这四个数组
+        # 而left_pre[0],right_pre[0]即为上一层根节点的左/右子节点
+        # 依此类推即可
+
+        # if len(preorder) == 0:
+        #     return None
+        #
+        # root = TreeNode(preorder[0])
+        # id_root = inorder.index(preorder[0])      # 可改进点：O(N),不可避免地每次遍历inorder寻找root所在位置 -> 解法三
+        # # left_pre = preorder[1 : ]               # 可改进点：以下四步将耗费不必要的计算时间 -> 解法二
+        # # left_in = inorder[: id_root]
+        # # right_pre = preorder[(id_root + 1) :]
+        # # right_in = inorder[id_root :]
+        # root.left = self.buildTree(preorder[1 : (id_root + 1)], inorder[: id_root])
+        # root.right = self.buildTree(preorder[(id_root + 1) :], inorder[id_root + 1 :])
+        #
+        # return root
 # leetcode submit region end(Prohibit modification and deletion)
